7287a503986792a51ed472e184ccf0fd2ed41453
unknown <diazc04080910@gmail.com>
2025-05-19 17:21:52 -0500

    feat: :sparkles: update the index's, style's and script's of ale

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const screens = ['screen1', 'screen2', 'screen3'];
+let index = 0;
+
+function showNextScreen() {
+  if (index > 0) {
+    document.getElementById(screens[index - 1]).style.display = 'none';
+  }
+
+  if (index < screens.length) {
+    document.getElementById(screens[index]).style.display = 'flex';
+    
+    // Solo continuar si no estamos en la última pantalla
+    if (index < screens.length - 1) {
+      setTimeout(showNextScreen, 2500);
+    }
+    
+    index++;
+  }
+}
+
+window.onload = showNextScreen;
+
